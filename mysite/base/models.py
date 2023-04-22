f022168991c6e378e3fecd43a2fcf8743a3bef35
langesh <langesh>
2023-04-22 12:38:14 +0530

    models

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
 # Create your models here.
+
+class Student(models.Model):
+    roll_no = models.IntegerField()
+    name = models.CharField(max_length=50)
+    destination = models.CharField(max_length=200)
+    source = models.CharField(max_length=200)
+
+    bus = models.ForeignKey("Bus",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Bus(models.Model):
+    bus_no = models.IntegerField()
+    destination = models.CharField(max_length=200)
+    driver = models.CharField(max_length=50)
+    source = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.bus_no)
+
--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bus
 
 # Create your views here.
 
 def base(request):
-    return HttpResponse("base")
+
+    bus = Bus.objects.all()
+
+    print(bus)
+
+    context = {
+        "data": bus
+    }
+
+    return render(request,"base/index.html",context)
